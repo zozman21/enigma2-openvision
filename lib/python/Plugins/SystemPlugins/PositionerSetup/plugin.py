2f909511626f1dbe984d8452e89c1fdd28efe103
Hains <hainsvdbosch@ziggo.nl>
2020-04-13 09:03:41 +0200

    PositionerSetup: lambda cannot take explicit tuple parameters

--- lib/python/Plugins/SystemPlugins/PositionerSetup/plugin.py
+++ lib/python/Plugins/SystemPlugins/PositionerSetup/plugin.py
@@ -980,7 +980,7 @@ class PositionerSetup(Screen):
 
 		def optimise(readings):
 			xi = readings.keys()
-			yi = map(lambda (x, y) : x, readings.values())
+			yi = map(lambda x_y : x_y[0], readings.values())
 			x0 = sum(map(mul, xi, yi)) / sum(yi)
 			xm = xi[yi.index(max(yi))]
 			return (x0, xm)
@@ -1115,7 +1115,7 @@ class PositionerSetup(Screen):
 
 		def optimise(readings):
 			xi = readings.keys()
-			yi = map(lambda (x, y) : x, readings.values())
+			yi = map(lambda x_y : x_y[0], readings.values())
 			x0 = int(round(sum(map(mul, xi, yi)) / sum(yi)))
 			xm = xi[yi.index(max(yi))]
 			return (x0, xm)
