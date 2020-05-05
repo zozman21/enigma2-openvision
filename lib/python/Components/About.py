077a0fd2007553324ae51a99ed23beef0f252e84
Hains <hainsvdbosch@ziggo.nl>
2020-05-05 12:11:04 +0200

    About.py: decode back to unicode string

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -289,7 +289,7 @@ def GetIPsFromNetworkInterfaces():
 	namestr = names.tostring()
 	ifaces = []
 	for i in range(0, outbytes, struct_size):
-		iface_name = bytes.decode(namestr[i:i+16]).split('\0', 1)[0].encode('ascii')
+		iface_name = bytes.decode(namestr[i:i+16]).split('\0', 1)[0].encode('ascii').decode('ascii')
 		if iface_name != 'lo':
 			iface_addr = socket.inet_ntoa(namestr[i+20:i+24])
 			ifaces.append((iface_name, iface_addr))
